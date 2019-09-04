b26f4356f12911210bf8aacd1937e4bc83bbbc2a
Yash Gupta <yg123yash@gmail.com>
2019-09-04 16:21:02 +0530

    Updates

---
+++ precache-manifest.997ead1f3041c613e45899ba38221cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75a693b71f5340a5ff2f48b72ae5c7c",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "9e2e0851534fa1cd7187",
+    "url": "/Contact-Manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1e9afaf7ad32b97c61b6",
+    "url": "/Contact-Manager/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9e2e0851534fa1cd7187",
+    "url": "/Contact-Manager/static/js/2.91b2fd1e.chunk.js"
+  },
+  {
+    "revision": "1e9afaf7ad32b97c61b6",
+    "url": "/Contact-Manager/static/js/main.504775e3.chunk.js"
+  },
+  {
+    "revision": "cc33fa19cf18e4a4ec4d",
+    "url": "/Contact-Manager/static/js/runtime~main.3ecb2d1f.js"
+  }
+]);
